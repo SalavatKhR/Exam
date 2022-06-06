ce14c219027896ad357093d5f9661b7d2b3eec22
SalavatKhR <SalavatKhR@gmail.com>
2022-06-06 21:03:07 +0300

    Initial commit

---
+++ Exam/ClientApp/src/App.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Route , Routes} from 'react-router';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+
+import './custom.css'
+
+export default class App extends Component {
+    static displayName = App.name;
+
+  render () {
+    return (
+        <Layout>
+            <Routes>
+                <Route path='/' element={<Home/>} />
+                {/*<Route path='/fetch-data' element={<FetchData/>} />*/}
+            </Routes>
+        </Layout>
+    );
+  }
+}
